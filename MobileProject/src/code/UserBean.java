62241884ac8f65c07cc84dfd8709a48c4db43ff8
Giblin91 <casinilorenzo91@gmail.com>
2015-04-23 14:40:10 +0200

    Added psw encryption

--- MobileProject/src/code/UserBean.java
+++ MobileProject/src/code/UserBean.java
@@ -1,5 +1,8 @@
 package code;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /* This is the JavaBean for the User, with its properties and the getters and setters */
 public class UserBean {
 	
@@ -16,7 +19,7 @@ public class UserBean {
 	}
 	
 	public void setPassword(String newPassword){
-		password = newPassword;
+		password = simpleEncrypt(newPassword);
 	}
 	
 	public String getPassword(){
@@ -29,5 +32,33 @@ public class UserBean {
 	
 	public boolean isValid(){
 		return valid;
+	}
+	
+	private String simpleEncrypt(String s){
+		String encryptedPsw = null;
+		
+		try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(s.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            encryptedPsw = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+		
+		return encryptedPsw;
 	}
 }
