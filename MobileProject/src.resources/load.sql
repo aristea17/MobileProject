c1f7ba83c374bb331f809921767bcb628383b42e
Giblin91 <casinilorenzo91@gmail.com>
2015-05-18 12:40:15 +0200

    new entry in DB, with more product, suppliers and categories than EVA!

--- MobileProject/src.resources/load.sql
+++ MobileProject/src.resources/load.sql
@@ -1,6 +1,6 @@
 -- DEPARTMENT --
 --1
-insert into department (d_id, d_name) values(1, 'administrator');
+insert into department (d_id, d_name) values(1, 'administration');
 
 insert into department (d_id, d_name) values(2, 'kitchen');
 
@@ -9,7 +9,7 @@ insert into department (d_id, d_name) values(3, 'clean');
 
 -- USERS --
 
-insert into users (u_id, department_d_id, psw, username) values(1, 1, '21232f297a57a5a743894a0e4a801fc3', 'administrator');
+insert into users (u_id, department_d_id, psw, username) values(1, 1, '21232f297a57a5a743894a0e4a801fc3', 'administration');
 
 insert into users (u_id, department_d_id, psw, username) values(2, 2, '09228dac155633b13780552bc01dc2e0', 'kitchen');
 
@@ -18,72 +18,202 @@ insert into users (u_id, department_d_id, psw, username) values(3, 3, '123402c04
 
 -- PRODUCTS --
 --1
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(1, 'latte mila', 'frigo', '1 litro', 30, 30);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(1, 'mila_latte_fresco', 'frigo', '1 litro', 30, 30);
 
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(2, 'yogurt mila', 'frigo', '1', 20, 20);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(2, 'mila_yogurt_vasetto_6_pack', 'frigo', '6', 20, 20);
 
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(3, 'latte vipiteno', 'frigo', '1', 20, 20);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(3, 'mila_latte_parz_scremo', 'frigo', '0.5 litro', 20, 20);
 
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(4, 'mele marlene', 'frutta e verdura', '10 Kg', 2, 4);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(4, 'mele_golden_cassetta', 'frutta e verdura', 'cassetta', 5, 6);
 --5
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(5, 'sapone', 'pulizia e igene', '1', 30, 50);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(5, 'saponetta_bagno', 'igiene', '1', 50, 60);
 
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(6, 'vetril', 'pulizia e igene', '500 ml', 5, 6);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(6, 'detergente_pavimenti', 'pulizia', '500 ml', 5, 6);
 
-insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(7, 'penne sfera', 'cartoleria', '50', 1, 3);
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(7, 'penne_sfera', 'cartoleria', '50', 1, 3);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(8, 'sterzing_yogurt_vasetto_6_pack', 'frigo', '6', 20, 15);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(9, 'heineken_birra_33cl_3_pack', 'bevande e alcolici', '3', 20, 50);
+--10
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(10, 'coca_cola_lattina_33cl_6_pack', 'bevande e alcolici', '6', 15, 20);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(11, 'manzo_tagliata', 'carne e pesce', '200g', 10, 5);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(12, 'pollo_petto', 'carne e pesce', '300g', 15, 10);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(13, 'insalata_cappuccio_cassetta', 'frutta e verdura', 'cassetta', 10, 25);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(14, 'kit_igene_personale', 'igiene', '1', 60, 70);
+--15
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(15, 'kit_doccia_shampoo', 'igiene', '5cl', 50, 45);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(16, 'detergente_vetri', 'pulizia', '50cl', 30, 42);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(17, 'lavanderia_detersivo', 'lavanderia', '5 litri', 10, 11);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(18, 'lavanderia_sapone_marsiglia', 'lavanderia', '1 litro', 6, 18);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(19, 'lavanderia_ammorbidente', 'lavanderia', '1 litro', 20, 28);
+--20
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(20, 'dentifricio', 'igiene', '100cl', 30, 40);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(21, 'carta_risma_100', 'cartoleria', '100 fogli', 30, 83);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(22, 'toner_stampante_nero', 'stampante', '1 toner', 2, 4);
+
+insert into products(p_id, p_name, category, batch_amount, minimum, stored) values(23, 'toner_stampante_3color', 'stampante', '1 toner', 2, 4);
+
 
 
 -- SUPPLIERS --
 --1
-insert into suppliers(s_id, address, company, email, representative, telephone) values(1, '', 'Despar', 'peppa@pig.com', 'Peppa Pig', '');
+insert into suppliers(s_id, address, company, email, representative, telephone) values(1, '', 'Despar', 'supplies@despar.com', 'Kurt', '');
 
-insert into suppliers(s_id, address, company, email, representative, telephone) values(2, '', 'Poli', 'goku@tory.com', 'Goku', '');
+insert into suppliers(s_id, address, company, email, representative, telephone) values(2, '', 'Poli', 'supplies@poli.com', 'Mr. Poli', '');
 
-insert into suppliers(s_id, address, company, email, representative, telephone) values(3, '', 'Carrefour', 'shepard@bioware.com', 'Commander Shepard', '');
+insert into suppliers(s_id, address, company, email, representative, telephone) values(3, '', 'Carrefour', 'info@carrefour.com', 'Commander Shepard', '');
 
-insert into suppliers(s_id, address, company, email, representative, telephone) values(4, '', 'Athesia', 'pens@production.com', '', '');
+insert into suppliers(s_id, address, company, email, representative, telephone) values(4, '', 'Athesia', 'order@athesia.com', '', '');
+--5
+insert into suppliers(s_id, address, company, email, representative, telephone) values(5, 'Via Macello, 42, Bolzano', 'Macelleria/Metzgerei', 'macello@metzger.com', 'The Butcher', '0471.166.166');
+
+insert into suppliers(s_id, address, company, email, representative, telephone) values(6, '', 'Acqua & Sapone', 'acqua@sapone.com', '', '');
 
 
 -- SELLS -- RELATION -- 
-
+-- Per Supplier
+-- 1 - Despar
 insert into sells (price, p_id, s_id) values(1.00, 1, 1);
 
-insert into sells (price, p_id, s_id) values(0.45, 2, 1);
+insert into sells (price, p_id, s_id) values(4.99, 2, 1);
 
-insert into sells (price, p_id, s_id) values( 0.49, 3, 1);
+insert into sells (price, p_id, s_id) values(0.68, 3, 1);
 
-insert into sells (price, p_id, s_id) values(30.00, 4, 1);
+insert into sells (price, p_id, s_id) values(10.00, 4, 1);
 
+insert into sells (price, p_id, s_id) values(0.89, 5, 1);
+
+insert into sells (price, p_id, s_id) values(5.00, 6, 1);
+
+insert into sells (price, p_id, s_id) values(4.80, 8, 1);
+
+insert into sells (price, p_id, s_id) values(3.69, 9, 1);
+
+insert into sells (price, p_id, s_id) values(3.81, 10, 1);
+
+insert into sells (price, p_id, s_id) values(1.96, 12, 1);
+
+insert into sells (price, p_id, s_id) values(6.00, 13, 1);
+
+insert into sells (price, p_id, s_id) values(12.36, 19, 1);
+
+insert into sells (price, p_id, s_id) values(1.30, 20, 1);
+-- 2 - Poli
 insert into sells (price, p_id, s_id) values(0.88, 1, 2);
 
-insert into sells (price, p_id, s_id) values(0.40, 2, 2);
+insert into sells (price, p_id, s_id) values(3.65, 2, 2);
 
 insert into sells (price, p_id, s_id) values(0.61, 3, 2);
 
+insert into sells (price, p_id, s_id) values(0.49, 7, 2);
+
+insert into sells (price, p_id, s_id) values(2.45, 9, 2);
+
+insert into sells (price, p_id, s_id) values(0.99, 12, 2);
+
+insert into sells (price, p_id, s_id) values(1.12, 16, 2);
+-- 3 - Carrefour
 insert into sells (price, p_id, s_id) values(0.85, 5, 3);
 
 insert into sells (price, p_id, s_id) values(2.00, 6, 3);
 
+insert into sells (price, p_id, s_id) values(1.50, 16, 3);
+
+insert into sells (price, p_id, s_id) values(6.89, 17, 3);
+
+insert into sells (price, p_id, s_id) values(4.32, 18, 3);
+
+insert into sells (price, p_id, s_id) values(5.23, 19, 3);
+
+insert into sells (price, p_id, s_id) values(0.85, 20, 3);
+-- 4 - Athesia
 insert into sells (price, p_id, s_id) values(2.50, 7, 4);
+
+insert into sells (price, p_id, s_id) values(4.00, 21, 4);
+
+insert into sells (price, p_id, s_id) values(15.00, 22, 4);
+
+insert into sells (price, p_id, s_id) values(20.00, 23, 4);
+-- 5 - Macelleria/Metzgerei
+insert into sells (price, p_id, s_id) values(3.65, 11, 5);
+
+insert into sells (price, p_id, s_id) values(1.86, 12, 5);
+-- 6 - Acqua & Sapone
+insert into sells (price, p_id, s_id) values(0.98, 5, 6);
+
+insert into sells (price, p_id, s_id) values(2.88, 14, 6);
+
+insert into sells (price, p_id, s_id) values(2.56, 15, 6);
+
+insert into sells (price, p_id, s_id) values(1.15, 20, 6);
 
 
 -- NEEDS -- RELATION --
+-- Per Department
+-- 1 - Administration
+insert into needs (d_id, p_id) values(1, 7);
 
+insert into needs (d_id, p_id) values(1, 21);
+
+insert into needs (d_id, p_id) values(1, 22);
+
+insert into needs (d_id, p_id) values(1, 23);
+-- 2 - -Kitchen
 insert into needs (d_id, p_id) values(2, 1);
 
 insert into needs (d_id, p_id) values(2, 2);
 
 insert into needs (d_id, p_id) values(2, 3);
 
-insert into needs (d_id, p_id) values(2, 4);
-
 insert into needs (d_id, p_id) values(2, 7);
 
+insert into needs (d_id, p_id) values(2, 4);
+
+insert into needs (d_id, p_id) values(2, 8);
+
+insert into needs (d_id, p_id) values(2, 9);
+
+insert into needs (d_id, p_id) values(2, 10);
+
+insert into needs (d_id, p_id) values(2, 11);
+
+insert into needs (d_id, p_id) values(2, 12);
+
+insert into needs (d_id, p_id) values(2, 13);
+
+
+-- 3 - Clean
 insert into needs (d_id, p_id) values(3, 5);
 
 insert into needs (d_id, p_id) values(3, 6);
 
 insert into needs (d_id, p_id) values(3, 7);
+
+insert into needs (d_id, p_id) values(3, 14);
+
+insert into needs (d_id, p_id) values(3, 15);
+
+insert into needs (d_id, p_id) values(3, 16);
+
+insert into needs (d_id, p_id) values(3, 17);
+
+insert into needs (d_id, p_id) values(3, 18);
+
+insert into needs (d_id, p_id) values(3, 19);
+
+insert into needs (d_id, p_id) values(3, 20);
+
 
 -- DEPTSUP -- RELATION --
 insert into deptsup (d_id, s_id) values (1, 4);
@@ -94,6 +224,10 @@ insert into deptsup (d_id, s_id) values (2, 2);
 
 insert into deptsup (d_id, s_id) values (2, 4);
 
+insert into deptsup (d_id, s_id) values (2, 5);
+
 insert into deptsup (d_id, s_id) values (3, 3);
 
 insert into deptsup (d_id, s_id) values (3, 4);
+
+insert into deptsup (d_id, s_id) values (3, 6);
